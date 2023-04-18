df0bf2db0d5dc6d2879261bfc94d44808164ffbd
Valerio Billi <valerio.billi18@gmail.com>
2023-04-18 15:51:34 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let mail = prompt("Inserisci la tua email")
+
+const mailValide = ["mariorossi@gmail.com", "lucaverdi@gmail.com", "paolobianchigmail.com", "chiarascura@gmail.com"]
+
+console.log(mailValide)
+
