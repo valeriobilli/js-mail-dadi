01dc182d99934b34bcc0c31a17276ea3dfdc9929
Valerio Billi <valerio.billi18@gmail.com>
2023-04-18 16:23:00 +0200

    funzionamento base

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-let mail = prompt("Inserisci la tua email")
-
 const mailValide = ["mariorossi@gmail.com", "lucaverdi@gmail.com", "paolobianchigmail.com", "chiarascura@gmail.com"]
 
-console.log(mailValide)
+let mail = prompt("Inserisci la tua email")
+
+if (mailValide.includes(mail)) {
+    console.log("L'email è corretta!")
+} else console.log ("Devi prima registrare l'email per poter accedere.")
+
 
