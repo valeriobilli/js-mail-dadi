e0698f46825d12e75b0c1e4eca655e7da985b833
Valerio Billi <valerio.billi18@gmail.com>
2023-04-19 16:29:32 +0200

    correzione lezione

--- js/main.js
+++ js/main.js
@@ -1,9 +1,50 @@
-const mailValide = ["mariorossi@gmail.com", "lucaverdi@gmail.com", "paolobianchigmail.com", "chiarascura@gmail.com"]
+let listaMailAutorizzate = ["mariorossi@gmail.com", "lucaverdi@gmail.com", "paolobianchigmail.com", "chiarascura@gmail.com"];
+let accessoConsentito;
+let mailUtente;
+const msgOkElem = document.getElementById("messaggioOK");
+const msgKoElem = document.getElementById("messaggioKO");
+const btn = document.getElementById("loginButton");
 
-let mail = prompt("Inserisci la tua email")
 
-if (mailValide.includes(mail)) {
-    console.log("L'email è corretta!")
-} else console.log ("Devi prima registrare l'email per poter accedere.")
+btn.addEventListener("click", function() {
+    accessoConsentito = false;
+    console.log("Lista email autorizzate", listaMailAutorizzate);
+    mailUtente = document.getElementById("emailUtente").value;
+    console.log("Email inserita: ", mailUtente);
+   
+    for (let c = 0; c < listaMailAutorizzate.length; c++) {
+        const mailDaConfrontare = listaMailAutorizzate[c];
+    
+        if(mailUtente == mailDaConfrontare) {
+            accessoConsentito = true;
+        } 
+    }
+
+    console.log("Valore variabile di controllo: ", accessoConsentito);
+
+    if(accessoConsentito == true) {
+        console.log("Benvenuto utente autorizzato!");
+        msgOkElem.classList.remove("hidden");
+        msgKoElem.classList.add("hidden");
+
+    } else {
+        console.log("Impossibile effetturare l'accesso, è necessario prima effetturare la registrazione.");
+        msgOkElem.classList.add("hidden");
+        msgKoElem.classList.remove("hidden");
+    }
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
